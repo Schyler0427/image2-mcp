9f5a874170329f89101f3a1e6116c448134eb12c
joy <scudderinell@gmail.com>
2026-08-07 19:24:46 +0800

    fix: normalize Windows redirected-input preambles

--- install.ps1
+++ install.ps1
@@ -187,8 +187,19 @@ function Read-KeyOnce {
     }
   }
 
-  if ($Value.StartsWith([char]0xFEFF)) {
-    $Value = $Value.Substring(1)
+  if ($InputIsRedirected) {
+    $MojibakeBom = -join @([char]226, [char]8226, [char]8212, [char]226, [char]8221, [char]144)
+    while ($Value.Length -gt 0) {
+      if ($Value[0] -eq [char]0xFEFF) {
+        $Value = $Value.Substring(1)
+        continue
+      }
+      if ($Value.StartsWith($MojibakeBom, [StringComparison]::Ordinal)) {
+        $Value = $Value.Substring($MojibakeBom.Length)
+        continue
+      }
+      break
+    }
   }
   if ([string]::IsNullOrWhiteSpace($Value)) {
     throw "API Key cannot be blank"
